f21ec69489f924d5674ee1c5870fbdb5052b461e
Geniroh <irochibuzor@gmail.com>
2022-04-02 01:32:33 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World I am Irobuisi Chibuzor");
+            Console.WriteLine("I am in the backend track of Wella schools training");
+
+            Console.WriteLine("I am a frontend developer looking to break forth into the backend space. I believe this is a great opportunity to do so and learn from some of the best in the field");
+        }
+    }
+}
